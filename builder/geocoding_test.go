d83962cf06157f090c02873b814ad100e664dca4
Artem Nikitin <artemnikitin@artemnikitin.com>
2016-03-17 16:34:05 +0100

    Small cleanup for tests

--- builder/geocoding_test.go
+++ builder/geocoding_test.go
@@ -7,8 +7,7 @@ import (
 
 func TestGeocodingUrl(t *testing.T) {
 	expected := "http://aa.bb/6.2/search.json?app_id=xx&app_code=yy&searchtext=sdf+dfdf+qwe&gen=9"
-	builder := NewGeocodingService()
-	url := builder.SetHost("http://aa.bb").SetAppID("xx").SetAppToken("yy").SetSearchPhrase("sdf dfdf qwe").Build()
+	url := NewGeocodingService().SetHost("http://aa.bb").SetAppID("xx").SetAppToken("yy").SetSearchPhrase("sdf dfdf qwe").Build()
 	if url != expected {
 		log.Println(url)
 		t.Error("Strings should be equal")
--- builder/map_tile_test.go
+++ builder/map_tile_test.go
@@ -7,8 +7,7 @@ import (
 
 func TestMapTileUrl(t *testing.T) {
 	expected := "http://example.com/mia/1.6/mapview?app_id=xxx&app_code=yyy&c=11.11,22.22&h=12&w=23&ppi=1&z=18&u=10"
-	builder := NewMapTileService()
-	builder = builder.SetHost("http://example.com").SetAppID("xxx").SetAppToken("yyy")
+	builder := NewMapTileService().SetHost("http://example.com").SetAppID("xxx").SetAppToken("yyy")
 	url := builder.SetLatitude(11.11).SetLongitude(22.22).SetWidth(23).SetHeight(12).SetDpi(1).Build()
 	if url != expected {
 		log.Println(url)
