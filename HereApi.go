1877c3643219965e6bf11f39f34200488aa72b7c
Artem Nikitin <artemnikitin@artemnikitin.com>
2016-06-12 19:01:36 +0200

    Small refactoring

--- HereApi.go
+++ HereApi.go
@@ -16,8 +16,9 @@ import (
 
 // GetCoordinates implements HERE Geocoding API for convert text address to GPS coordinates
 func GetCoordinates(appID, appToken, searchText string) string {
-	builder := builder.NewGeocodingService().SetAppID(appID).SetAppToken(appToken)
-	url := builder.SetSearchPhrase(searchText).Build()
+	url := builder.NewGeocodingService().
+		SetAppID(appID).SetAppToken(appToken).
+		SetSearchPhrase(searchText).Build()
 	resp := sendRequest(url)
 	defer closeAfter(resp)
 	if resp.StatusCode != 200 {
@@ -30,8 +31,10 @@ func GetCoordinates(appID, appToken, searchText string) string {
 
 // GetPicture returns map tile for specific set of GPS coordinates
 func GetPicture(appID, appToken string, lat, lon float64, h, w, dpi int) []byte {
-	builder := builder.NewMapTileService().SetAppID(appID).SetAppToken(appToken)
-	url := builder.SetLatitude(lat).SetLongitude(lon).SetWidth(w).SetHeight(h).SetDpi(dpi).Build()
+	url := builder.NewMapTileService().
+		SetAppID(appID).SetAppToken(appToken).
+		SetLatitude(lat).SetLongitude(lon).
+		SetWidth(w).SetHeight(h).SetDpi(dpi).Build()
 	resp := sendRequest(url)
 	defer closeAfter(resp)
 	if resp.StatusCode != 200 {
